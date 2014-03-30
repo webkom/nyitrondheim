f046617777c412079bce9a142c4e1f18f2a15c54
ekmartin <ekmartinb@gmail.com>
2014-03-30 15:56:21 +0200

    Move frontpage function into articles

--- config/routes.js
+++ config/routes.js
@@ -1,19 +1,12 @@
 var frontpage = require('../app/controllers/frontpage.js')
-    ,articles  = require('../app/controllers/articles.js')
-    ,unions    = require('../app/controllers/unions.js');
-
-
-
-
+  , articles  = require('../app/controllers/articles.js')
+  , unions    = require('../app/controllers/unions.js')
+  , url       = require('url')
+  ;
 module.exports = function(app) {
-
-
-  app.get('/api/frontpage', frontpage.getUnionArticles);
-
-  app.get('/api/articles/:slug', articles.getArticles);
-
   app.get('/api/unions/:union', unions.getUnion);
 
+  app.get('/api/unions/:union/articles', articles.getUnionArticles);
 
-
+  app.get('/api/unions/:union/articles/:slug', articles.getArticleBySlug);
 };
