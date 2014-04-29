ded8c10d7e75ae06429e54d0a1ff6b94a596bfef
ekmartin <ekmartinb@gmail.com>
2014-04-29 17:12:14 +0200

    Add calendar to /calendar, style it and bind it to the events added from /admin.

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -2,7 +2,7 @@ require('./services');
 require('./controllers');
 require('./directives');
 
-var app = angular.module('nitApp', ['ngRoute', 'ngAnimate', 'nitControllers', 'nitDirectives', 'nitServices', 'ngQuickDate']);
+var app = angular.module('nitApp', ['ngRoute', 'ngAnimate', 'nitControllers', 'nitDirectives', 'nitServices', 'ui.calendar', 'ui.bootstrap']);
 
 app.config(function($routeProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
@@ -19,17 +19,4 @@ app.config(function($routeProvider, $locationProvider) {
      templateUrl: 'partials/front',
      controller: 'PagesController'
    });
-});
-
-app.config(function(ngQuickDateDefaultsProvider) {
-  // Configure with icons from font-awesome
-  return ngQuickDateDefaultsProvider.set({
-    closeButtonHtml: '<i class="fa fa-times"></i>',
-    buttonIconHtml: '<i class="fa fa-clock-o"></i>',
-    nextLinkHtml: '<i class="fa fa-chevron-right"></i>',
-    prevLinkHtml: '<i class="fa fa-chevron-left"></i>',
-    dayAbbreviations: ['Sø', 'Ma', 'Ti', 'On', 'To', 'Fr', 'Lø'],
-    dateFormat: 'dd/MM/yyyy',
-    disableTimepicker: true
-  });
 });
--- app/assets/js/controllers.js
+++ app/assets/js/controllers.js
@@ -30,33 +30,85 @@ nitControllers.controller('MainController',
 
 nitControllers.controller('CalendarController',
   ['$scope', 'articleService', 'unionService', function($scope, articleService, unionService) {
+  $scope.uiConfig = {
+    calendar: {
+      height: 450,
+      editable: false,
+      dayNames: ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'],
+      dayNamesShort: ['Sø', 'Ma', 'Ti', 'On', 'To', 'Fr', 'Lø'],
+      header: {
+        left: 'month agendaWeek agendaDay',
+        center: 'title',
+        right: 'today prev,next'
+      },
+      allDaySlot: true,
+      allDayText: 'all-day',
+      firstHour: 6,
+      slotMinutes: 30,
+      defaultEventMinutes: 120,
+      axisFormat: 'H:mm', //,'h(:mm)tt',
+      timeFormat: 'H:mm',
+      columnFormat: {
+        month: 'ddd',
+        week: 'ddd dd/MM',
+        day: 'dddd dd/MM'
+      },
+      titleFormat: {
+        month: 'MMMM yyyy',
+        week: 'dd. MMM [ yyyy]{ &#8212; dd.[ MMM] yyyy}',
+        day: 'dddd, d. MMM yyyy'
+      },
+      buttonText: {
+        today: 'I dag',
+        month: 'Måned',
+        week: 'Uke',
+        day: 'Dag'
+      },
+      dragOpacity: {
+          agenda: 0.5
+      },
+      minTime: 0,
+      maxTime: 24
+    }
+  };
+
+  $scope.generalEvents = [];
+  $scope.unionEvents = [];
+  $scope.eventSources = [$scope.generalEvents, $scope.unionEvents];
+
   articleService.findAllEvents(unionService.last()._id)
     .success(function(events) {
-      $scope.unionEvents = events.map(function(e) {
-        return {
-          id: e._id,
-          text: e.title,
-          start: e.startDate,
-          end: e.endDate
-        };
+      events.forEach(function(e) {
+        $scope.unionEvents.push({
+          title: e.title,
+          start: new Date(e.start),
+          end: new Date(e.end),
+          url: '/' + e.slug,
+          allDay: false,
+          color: '#ff7c82'
+        });
       });
     })
     .then(articleService.findAllEvents('general')
       .success(function(events) {
-        $scope.generalEvents = events.map(function(e) {
-          return {
-            id: e._id,
-            text: e.title,
-            start: e.startDate,
-            end: e.endDate
-          };
+        events.forEach(function(e) {
+          $scope.generalEvents.push({
+            title: e.title,
+            start: new Date(e.start),
+            end: new Date(e.end),
+            url: '/' + e.slug,
+            allDay: false,
+            color: '#ff3e4c'
+          });
         });
       })
       .finally(function() {
         console.log("got to finally", $scope.unionEvents, $scope.generalEvents);
-        $scope.events = $scope.unionEvents.concat($scope.generalEvents);
+        $scope.eventSources = [$scope.unionEvents, $scope.generalEvents];
+        console.log("her",$scope.eventSources);
       })
     );
+
 }]);
 
 nitControllers.controller('PageController',
