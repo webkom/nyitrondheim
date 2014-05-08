44ff20c16efe04111cbb37d1f762be4500236f66
Hanse <hanse19@gmail.com>
2014-05-08 17:38:43 +0200

    Remove ngAnimate.

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -3,7 +3,7 @@ require('./controllers');
 require('./directives');
 
 var app = angular.module('nitApp',
-  ['ngRoute', 'ngAnimate', 'nitControllers', 'nitDirectives', 'nitServices', 'ui.calendar', 'ui.bootstrap', 'textAngular', 'angularFileUpload']);
+  ['ngRoute', 'nitControllers', 'nitDirectives', 'nitServices', 'ui.calendar', 'ui.bootstrap', 'textAngular', 'angularFileUpload']);
 
 app.config(function($routeProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
@@ -55,4 +55,3 @@ app.filter('exists', function() {
     return obj !== null && obj !== undefined && obj !== 'null' && obj !== 'undefined';
   };
 });
-
