21902851df3c2cf937ce610a566f5f696e7e9981
Hanse <hanse19@gmail.com>
2014-05-09 01:21:43 +0200

    Add prerender.io service.

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(passport.session());
 
 app.use(express.static(__dirname + '/app/assets/vendor'));
 app.use(express.static(__dirname + '/public'));
+app.use(require('prerender-node'));
 
 app.locals.pretty = true;
 
