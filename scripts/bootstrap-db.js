062112e9cbdf2f05d5d40b5d93e190f25777d7bb
ekmartin <ekmartinb@gmail.com>
2014-05-11 14:45:00 +0200

    Add a command to bootstrap only articles, not unions.

--- scripts/bootstrap-db.js
+++ scripts/bootstrap-db.js
@@ -52,7 +52,7 @@ program
   .description('Bootstraps the database with a set of unions from various schools in Trondheim.')
   .option('-a, --articles', 'Include example articles.')
   .option('-c, --clear', 'Clears the database before bootstrapping.')
-  .option('-f, --force', 'Needed to bootstrap a non-localhost database.')
+  .option('-f, --force', 'Needed to bootstrap a non-localhost database, or to bootstrap a non-empty database.')
   .action(function(database, options) {
     if (!database) {
       database = 'mongodb://localhost:27017';
@@ -68,6 +68,34 @@ program
           });
         }
         else createUnions(options.articles);
+      });
+    }
+    else {
+      console.log('--force is needed to bootstrap a non-localhost database.');
+    }
+  });
+
+program
+  .command('articles [database]')
+  .description('Bootstraps the database with example articles.')
+  .option('-f, --force', 'Needed to bootstrap a non-localhost database, or to bootstrap a non-empty database.')
+  .action(function(database, options) {
+    if (!database) {
+      database = 'mongodb://localhost:27017';
+      console.log('No database specified, setting database to localhost.');
+    }
+    if (options.force || _.contains(database, 'localhost')) {
+      mongoose.connect(database, function(err) {
+        if (err) console.log('Couldn\'t connct to database, database');
+        Article.find({}, function(err, articles) {
+          if (err) console.log('Couldn\'t find articles.', err);
+          if (!articles.length || options.force) {
+            Union.find({}, function(err, unions) {
+              if (err) console.log('Couldn\'t find any unions.', err);
+              async.each(unions, addArticles, done);
+            });
+          }
+        });
       });
     }
     else {
