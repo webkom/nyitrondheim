d1a6e82e5d7e40b6b4c0f32c7ce8e093c790c1d6
Odin Ugedal <odin@ugedal.com>
2017-08-08 14:03:50 +0200

    Add proper logging of mail errors
    This makes it possible to debug #68

--- app/mail/index.js
+++ app/mail/index.js
@@ -1,16 +1,21 @@
 var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
 
-var transporter = nodemailer.createTransport(smtpTransport({
-  port: process.env.SMTP_PORT,
-  host: process.env.SMTP_HOST,
-  secure: false,
-  ignoreTLS: true
-}));
+var transporter = nodemailer.createTransport(
+  smtpTransport({
+    port: process.env.SMTP_PORT,
+    host: process.env.SMTP_HOST,
+    secure: false,
+    ignoreTLS: true
+  })
+);
 
 exports.sendMail = function(options, callback) {
   if (process.env.SEND_MAIL) {
-    return transporter.sendMail(options, callback);
+    return transporter.sendMail(options, function(error, info) {
+      if (error) console.log(error);
+      return callback(error, info);
+    });
   }
   callback();
 };
