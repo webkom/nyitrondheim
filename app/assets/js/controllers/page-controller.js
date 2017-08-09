1ba9c7fb0f5cbc2dd753a56e6d20c39a5e699021
Odin Ugedal <odin@ugedal.com>
2017-08-09 18:59:02 +0200

    Add last modified to articles

--- app/assets/js/controllers/page-controller.js
+++ app/assets/js/controllers/page-controller.js
@@ -1,12 +1,21 @@
 /**
  * PageController
  */
- 
+
 module.exports = ['$rootScope', '$scope', '$location', '$routeParams', 'articleService', function($rootScope, $scope, $location, $routeParams, articleService) {
   $scope.loading = true;
   $scope.article = {};
 
   $rootScope.title = '';
+  $scope.dateToHumanReadable = function(date) {
+    var mm = date.getMonth() + 1;
+    var dd = date.getDate();
+    return [
+      (dd < 10 ? '0' : '') + dd,
+      (mm < 10 ? '0' : '') + mm,
+      date.getFullYear()
+    ].join('.');
+  };
 
   $scope.update = function() {
     $scope.loading = true;
@@ -15,6 +24,10 @@ module.exports = ['$rootScope', '$scope', '$location', '$routeParams', 'articleS
       articleService.findBySlug(union, $routeParams.articleSlug).success(function(article) {
         $scope.article = article;
         $rootScope.title = $scope.article.title;
+
+        $scope.lastModified = $scope.dateToHumanReadable(
+          new Date(article.lastModified || article.createdAt)
+        );
         $scope.loading = false;
       }).error(function() {
         $scope.article.title = '404';
