ca45f65bf067cb26758b85fb1cac360207691970
Olav Kihle <olavki@stud.ntnu.no>
2022-11-09 23:05:16 +0100

    Add nextui, boilerplate splash page
    Co-authored-by: jennymul <jennymul@users.noreply.github.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/global.css';
-import { AppProps } from 'next/app';
+import type { AppProps } from 'next/app';
+import { NextUIProvider } from '@nextui-org/react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
+
+export default MyApp;
