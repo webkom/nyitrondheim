0437da1070b1b33431494fd0e4fdae66bdcf3c66
Olav Kihle <olavkihle@gmail.com>
2022-11-16 21:48:09 +0100

    Test parallax effects

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Button } from '@nextui-org/react';
 import styles from '../styles/Home.module.css';
+import Header from './header';
+import MainContent from './mainContent';
+import SplashPage from './splashPage';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Ny i Trondheim</title>
         <meta name="description" content="Ny i Trondheim" />
@@ -13,9 +16,23 @@ const Home: NextPage = () => {
       </Head>
 
       <div className={styles.main}>
+        <Parallax pages={1.4} style={{ height: '100vh' }}>
+          <ParallaxLayer offset={0} speed={-1.1} factor={0.1}>
+            <div className={styles.container}>
               <Header />
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0.099} speed={2} factor={0.2}>
+            <div className={styles.container}>
               <SplashPage />
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0.7} speed={0.3}>
+            <div className={styles.container}>
               <MainContent />
+            </div>
+          </ParallaxLayer>
+        </Parallax>
       </div>
     </div>
   );
