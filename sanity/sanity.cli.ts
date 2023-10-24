241c6713ae3ccc0f4fd4e644bebadd2922c92895
Jonatan Norbye <jonatan@norbye.com>
2023-10-25 05:08:36 +1300

    Move sanity into base project
    Now both projects share dependencies

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
-    projectId: 'e0ffh349',
-    dataset: 'production',
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   },
-})
+});
